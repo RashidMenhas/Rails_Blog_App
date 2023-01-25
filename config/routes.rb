0b6fd6a0463b0ee44a2cda150c093f9070b432e6
Rashid Menhas <rashidmenhas10@gmail.com>
2023-01-25 17:00:08 +0500

    Set routes in routes.rb file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   
   root "users#index"
-
-  get '/posts/new', to: 'posts#new', as: 'create_post'
-  post '/posts/new', to: 'posts#create'
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'post'
-  post '/users/:user_id/posts/:id/', to: 'posts#create_comment'
-  post '/users/:user_id/posts/:id/likes', to: 'posts#create_like', as: 'like'
-
-  resources :users, only: [:index, :show] do 
-    resources :posts, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create] 
+      resources :likes, only: [:new, :create]
+    end
   end
 end
