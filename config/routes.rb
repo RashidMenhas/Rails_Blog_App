005cc131898553afffe2249b4fa74f40f9dceb1e
Rashid Menhas <rashidmenhas10@gmail.com>
2023-02-05 21:38:40 +0500

    set routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   
-  root to: 'users#index'
+  root "users#index"
 
-  resources :users, only: [:index, :show] do
+  get '/posts/new', to: 'posts#new', as: 'create_post'
+  post '/posts/new', to: 'posts#create'
+  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'post'
+  post '/users/:user_id/posts/:id/', to: 'posts#create_comment'
+  post '/users/:user_id/posts/:id/likes', to: 'posts#create_like', as: 'like'
+
+  resources :users, only: [:index, :show] do 
     resources :posts, only: [:index, :show]
   end
 end
