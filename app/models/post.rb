9f72ffb96eced8ce7c683350f7fd8e4ac0c30c70
Rashid Menhas <rashidmenhas10@gmail.com>
2023-02-11 14:45:56 +0500

    post model validatable and affence free.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
   after_save :update_posts_counter
 
-  validates :title, presence:true, length:{ in:1..250 }
-  validates :comments_counter, :likes_counter, numericality: {only_integer: true}, comparison: { greater_than_or_equal_to: 0} 
-
+  validates :title, presence: true, length: { in: 1..250 }
+  validates :comments_counter, :likes_counter, numericality: { only_integer: true },
+                                               comparison: { greater_than_or_equal_to: 0 }
 
   belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
