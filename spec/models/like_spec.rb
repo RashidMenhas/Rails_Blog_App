dc7264f66182af877d0a78e7831f9f204097bafc
Rashid Menhas <rashidmenhas10@gmail.com>
2023-02-11 14:48:34 +0500

    Make a unit test and check test for like.

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do
   before :each do
-    @author = User.new(name:'Menhas', photo:'https://res.cloudinary.com/highereducation/images/w_1024,h_683,c_scale/f_auto,q_auto/v1669994566/BestColleges.com/web-developer-vs-software-engineer-man-with-laptop/web-developer-vs-software-engineer-man-with-laptop-1024x683.jpg', bio:'Full-stack software developer')
+    @author = User.new(name: 'Menhas',
+                       photo: 'https://res.cloudinary.com/highereducation/images/w_1024,h_683,c_scale/f_auto,q_auto/v1669994566/BestColleges.com/web-developer-vs-software-engineer-man-with-laptop/web-developer-vs-software-engineer-man-with-laptop-1024x683.jpg', bio: 'Full-stack software developer')
 
     @post = Post.new(author: @author, title: 'Post for testing', text: 'This post is for testing')
 
@@ -13,7 +14,7 @@ RSpec.describe Like, type: :model do
     expect(@like).to_not respond_to(:update_likes_counter)
   end
 
-  it'will have the posts like_counter through update_comment_counter' do
+  it 'will have the posts like_counter through update_comment_counter' do
     expect(@post.likes_counter).to eq 1
   end
 end
