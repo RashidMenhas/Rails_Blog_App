6b9a60dd23a3eae18a03943b324a8cf0d99d5c9d
Rashid Menhas <rashidmenhas10@gmail.com>
2023-02-13 13:20:10 +0500

    Create validation title precense and numericiltiy integer length max 250.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,10 @@
 class Post < ApplicationRecord
   after_save :update_posts_counter
+
+  validates :title, presence:true, length:{ in:1..250 }
+  validates :comments_counter, :likes_counter, numericality: {only_integer: true}, comparison: { greater_than_or_equal_to: 0} 
+
+
   belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
