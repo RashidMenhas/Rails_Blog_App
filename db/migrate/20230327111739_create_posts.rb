60941db618494db9840523294db4e387f6bc2d1f
Rashid Menhas <rashidmenhas10@gmail.com>
2023-03-27 23:14:32 +0500

    change foreign key as a require.

--- db/migrate/20230327111739_create_posts.rb
+++ db/migrate/20230327111739_create_posts.rb
@@ -2,7 +2,6 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.references :author, null: false, foreign_key: { to_table: :users }
-      t.references :post, null: false, foreign_key: true
       t.string :title
       t.text :text
       t.integer :comments_counter
--- db/schema.rb
+++ db/schema.rb
@@ -26,14 +26,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_111836) do
 
   create_table "likes", force: :cascade do |t|
     t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
     t.bigint "author_id", null: false
-    t.bigint "post_id", null: false
     t.string "title"
     t.text "text"
     t.integer "comments_counter"
@@ -41,7 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_111836) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
-    t.index ["post_id"], name: "index_posts_on_post_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,7 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_111836) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
-  add_foreign_key "posts", "posts"
   add_foreign_key "posts", "users", column: "author_id"
 end
