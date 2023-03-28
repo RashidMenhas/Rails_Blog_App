789e3f9de9ac03d756ec6e693b191cf6b6bbf743
Rashid Menhas <rashidmenhas10@gmail.com>
2023-03-28 15:46:21 +0500

    make a post model.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,16 @@
 class Post < ApplicationRecord
-after_save :update_posts_counter
-belongs_to :author, class_name: 'User'
-has_many :comments, dependent: :destroy
-has_many :likes, dependent: :destroy
+  after_save :update_posts_counter
+  belongs_to :author, class_name: 'User'
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
-def recent_five
-comments.order(created_at: :desc).limit(5)
-end
+  def recent_five
+    comments.order(created_at: :desc).limit(5)
+  end
 
-private
+  private
 
-def update_posts_counter
-author.update(posts_counter: author.posts.all.length)
-end
-
+  def update_posts_counter
+    author.update(posts_counter: author.posts.all.length)
+  end
 end
