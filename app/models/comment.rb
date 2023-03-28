8cd2a54fa5695537e8a7383e23340a8d63c61186
Rashid Menhas <rashidmenhas10@gmail.com>
2023-03-28 15:46:31 +0500

    make a post model.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,9 @@
 class Comment < ApplicationRecord
-    after_save :update_comments_counter
-    belongs_to :author, class_name: 'User'
-belongs_to :post
+  after_save :update_comments_counter
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
-def update_comments_counter
-post.update(comments_counter: post.comments.all.length)
-end
-
+  def update_comments_counter
+    post.update(comments_counter: post.comments.all.length)
+  end
 end
