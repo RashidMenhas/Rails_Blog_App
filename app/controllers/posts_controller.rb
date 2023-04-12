b8ffe15ec86842ac7e282ffc4227da32bd5ec3f1
Rashid Menhas <rashidmenhas10@gmail.com>
2023-04-12 13:19:41 +0500

    Fix linter error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
-    @comment = Comment.new
+    @comments = @post.comments.includes([:author])
     @like = Like.new
   end
 
