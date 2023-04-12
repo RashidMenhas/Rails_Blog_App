c9901dcac2cbb851787ae86eb799ddc6c44fe68c
Rashid Menhas <rashidmenhas10@gmail.com>
2023-04-12 22:33:53 +0500

    fix linter errors.

--- spec/views/users/index_spec.rb
+++ spec/views/users/index_spec.rb
@@ -3,13 +3,13 @@ RSpec.describe 'User index', type: :feature do
   before :each do
     @avatar_pic = 'https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2022/10/Andor_Luthen-Rael.png?fit=2554,1642&amp;quality=60&amp;strip=all&amp;ssl=1'
     @author1 = User.create(name: 'Luthen Rael',
-                          photo: @avatar_pic,
-                          bio: 'A mysterious man with an eye for valuable finds, Luthen Rael will go to great lengths
+                           photo: @avatar_pic,
+                           bio: 'A mysterious man with an eye for valuable finds, Luthen Rael will go to great lengths
                           and traverse the galaxy to locate the pieces he needs to achieve his goals.')
 
     @author2 = User.create(name: 'Rashid',
-                          photo: '',
-                          bio: 'A mysterious man with an eye for valuable finds, Luthen Rael will go to great lengths
+                           photo: '',
+                           bio: 'A mysterious man with an eye for valuable finds, Luthen Rael will go to great lengths
                           and traverse the galaxy to locate the pieces he needs to achieve his goals.')
     visit users_path
   end
