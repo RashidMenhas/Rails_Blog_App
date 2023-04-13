e7ef533fea5752efed7e30549484161a67418029
Rashid Menhas <rashidmenhas10@gmail.com>
2023-04-13 22:31:02 +0500

    Change the routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   root "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
